06adaf76924bf8f02633562357ff2d0f556fca68
Justyna Bednarko <justyna.bednarko@gmail.com>
2023-04-15 00:39:23 +0200

    add modal and form

---
+++ src/components/AddressForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="item">
+      <label>Country: {{value.country}}</label>
+      <input 
+        type="text" 
+        :value="value.country" 
+        @input="onChangeCountry($event.target.value)"
+      />
+      <label>Street: {{value.street}}</label>
+      <input 
+        type="text" 
+        :value="value.street" 
+        @input="onChangeStreet($event.target.value)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddressForm',
+
+props: {
+    value: {
+      type: Object,
+      required: true
+    }
+  },
+
+  methods: {
+    onChangeCountry (value) {
+      const updatedValue = {...this.value, country: value};
+      console.log('this.value', this.value)
+      console.log('updatedValue', updatedValue)
+      this.$emit('input', updatedValue);
+    },
+    onChangeStreet (value) {
+      const updatedValue = {...this.value, street: value};
+      this.$emit('input', updatedValue);
+    }    
+  }
+
+
+}
+</script>
+
+<style>
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,53 @@
 <template>
   <div class="hello">
-    <button @click="showModal = true">Click</button>
-    {{showModal}}
+    <button @click="showModal = true">Open Modal</button>
+    <CustomModal 
+      @close="showModal = false"
+      v-if=showModal
+    >
+      <template #header>
+        <h2>Address</h2>
+      </template>
+      <template #body>
+        <AddressForm
+          v-model="address"
+        ></AddressForm>
+      </template>
+    </CustomModal>
+    <div class="item">address.country: {{address.country}}</div>
+    <div class="item">address.street: {{address.street}}</div>
   </div>
 </template>
 
 <script>
+import CustomModal from './CustomModal.vue'
+import AddressForm from './AddressForm.vue'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      showModal: false
+      showModal: false,
+      address: {
+        country: '',
+        street: ''
+      }
     }
   },
 
   props: {
     msg: String
+  },
+
+  components: {
+    CustomModal,
+    AddressForm    
   }
 }
 </script>
+
+<style>
+.item {
+  margin: 10px;
+}
+</style>
